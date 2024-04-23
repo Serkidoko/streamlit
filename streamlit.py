86470ce3e99aaf5b149a99a132d3309e3c5f5a86
Serkidoko <150759723+Serkidoko@users.noreply.github.com>
2024-04-23 11:40:09 +0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+
+filename = st.text_input('Enter a file path:')
+try:
+    with open(filename) as input:
+        st.text(input.read())
+except FileNotFoundError:
+    st.error('File not found.')
